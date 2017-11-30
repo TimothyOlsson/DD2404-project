e72beb9550332e790d9973c4bc8c135a55441ecb
Timothy <30318121+TimothyBergstrom@users.noreply.github.com>
2017-11-30 16:34:11 +0100

    Changed name of file and minor changes
    Changes:
    Load file can now load in two modes, looping if data is shorter than requested or cutting it.
    
    Instead of doing ord(c), I use 0 as 'A' and 90 as 'Z', which can be normalized in the training.
    
    Optimization for loading data

--- scripts/load_data.py
+++ scripts/load_data.py
@@ -2,17 +2,23 @@ import os
 import tarfile
 from Bio import SeqIO
 import numpy as np
+import math
 
-def load_training(train_length):
+def load_training(train_length, verbose=False):
     """Loads traning data
     Ignores files that starts with . since they are config files in ubuntu.
     """
-    x_train = np.empty((0, train_length))
-    y_train = np.empty((0, 1))
+    if verbose:
+        print('Loading data...')
+    
+    x = np.empty((0, train_length))
+    y = np.empty((0, 1))
     y_pos = np.array([1])
     y_neg = np.array([0])
     cur_dir = os.getcwd()
     os.chdir('../data/training_data')
+    data_counter = 0
+
     for (dirpath, dirnames, filenames) in os.walk(os.getcwd()):
         for filename in filenames:
             if filename.startswith('.'):
@@ -23,21 +29,39 @@ def load_training(train_length):
                 record = record.split('#')
                 seq = record[0]
                 region = record[1]
-                #NOTE: RIGHT NOW, YOU JUST TAKE FIRST X AND SKIPS THE REST
-                seq = [ord(x) for x in seq]
+
+                # ord('A') = 65
+                seq = [ord(x)-65 for x in seq]
+                
+                """#FILL WITH ZEROS, NOTE: RIGHT NOW, YOU JUST TAKE FIRST X AND FILLS REST WITH 0
                 if len(seq) < train_length:
                     for i in range(train_length - len(seq)):
                         seq.append(0)
+                """
+                
+                #LOOP SEQ
+                if len(seq) < train_length:
+                    seq = seq + seq*(math.floor(train_length - len(seq))) # Double list if possible
+                    fill = train_length - len(seq) # Find how many elements left to fill
+                    seq = seq + seq[:fill]
+                
+                        
                 seq = np.array(seq[:train_length])
-                x_train = np.vstack((x_train, seq))
+                x = np.vstack((x, seq))
                 if os.path.dirname(dirpath).endswith('positive_examples'):
-                    y_train = np.vstack((y_train, y_pos))
+                    y = np.vstack((y, y_pos))
                 else:
-                    y_train = np.vstack((y_train, y_neg))
+                    y = np.vstack((y, y_neg))
+                data_counter += 1
                 """Can be used in future to find which data was tm or not"""
                 #print(os.path.basename(dirpath))
+
+                
     os.chdir(cur_dir)
-    return x_train, y_train
+    if verbose:
+        print(f'Loaded {data_counter} data sets')
+        print('Pos values starts at: ' + str(np.argmax(y)))
+    return x, y
 
 def load_tar(): # WIP
     """Loads a tar file and creates a numpy array
