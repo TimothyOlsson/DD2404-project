53f622d39e3b923c12483ec1c7dba7c3bf9718ce
Timothy <30318121+TimothyBergstrom@users.noreply.github.com>
2017-12-01 12:20:18 +0100

    minor changes for normalization

--- scripts/Display data.py
+++ scripts/Display data.py
@@ -1,6 +1,6 @@
 from load_data import load_training
 
-x,y = load_training(50, verbose=True)
+x,y = load_training(50, verbose=True, resample_method='FIRST')
 data_set = 1334
 import matplotlib.pyplot as plt
 import time
@@ -9,7 +9,7 @@ for i in range(100):
     seq = x[data_set][:]
     val = y[data_set][:]
 
-    #seq /= 255
+    seq /= 90
 
     print(seq)
     print(val)
