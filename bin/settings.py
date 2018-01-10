2adfb00c99a01779681dfeeaafe0e88dd5dab312
Timothy <30318121+TimothyBergstrom@users.noreply.github.com>
2018-01-10 23:20:17 +0100

    Added more settings
    Added comments to some settings
    
    Added so you can choose model
    
    Added so you can limit the amount of samples
    
    Changed parameter names to prevent cluttering the namespace
    
    Added values to split data into train, validation and test set

--- bin/settings.py
+++ bin/settings.py
@@ -2,41 +2,47 @@
 have all the settings in a python file and import it? That is such a simple solution :)"""
 
 # GPU
-use_gpu = True
+use_gpu = True  # Only works if you have tensorflow-gpu installed
 
 # DATA
-load_saved_array = False
-data_folder = '../data/training_data'
-equalize_data = True
-save_array = True
-cutoff = 30
-data_augmentation = False
-fix_samples = 'IGNORE'
+load_saved_array = True  # Loads array from file
+data_folder = '../data/training_data'  # Folders are choosen relative from NN.py
+equalize_data = True  # Makes amount of negative and positive data equal
+save_array = True  # Save array to file
+seq_length = 30  # Length of sequence that will be analysed
+data_augmentation = False  # Will divide
+fix_samples = 'ZERO'
 vectorize = True
 use_ascii = False
 
 # MODEL
-load_saved_model = False
+load_saved_model = True
 train_loaded_model = False
+choosen_model = 2  #
+
+# TRAINING DATA
+train_test_split = 0.8  # Splits full data set into train and test set, percentage train:test
+train_val_split = 0.8  # Splits train data set into train and validation set, percentage train:validation
+limit_samples = 6000  # None if you don't want to limit the amount of samples
 
 # TRAINING
-lr = 0.005  # More parameters --> change lr
+lr = 0.0001  # More parameters --> change lr
 decay = 1.1e-5
 momentum = 0.5
-val_split = 0.7
 epochs = 3000
-batch_size = 258
+batch_size = 4096
 use_lr_scheduler = True
-lr_scheduler_plan = {0.88: 0.002, 0.92: 0.001, 0.93: 0.0005}  # REMEMBER TO CHANGE THIS WHEN CHANGING MODEL AND LR
+lr_scheduler_plan = {}  # REMEMBER TO CHANGE THIS WHEN CHANGING MODEL AND LR
 config_plot_realtime = True
-plot_realtime_interval = 10
+plot_realtime_interval = 1
 
 # PLOTTING
 plot_performance = False
 
 # GAN
 train_GAN = True
-load_trained_GAN = True
-GAN_lr = 0.03
+load_trained_GAN = False
+GAN_lr = 0.0001
 GAN_epochs = 3000
-GAN_batch_size = 256
+GAN_batch_size = 512
+GAN_limit_samples = None  # OBS: Depends on first limit samples
