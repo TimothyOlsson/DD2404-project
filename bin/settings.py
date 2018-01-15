76a10c7e7f7a53adaa9e12ab12b4dadb108cce14
Timothy <30318121+TimothyBergstrom@users.noreply.github.com>
2018-01-15 18:54:34 +0100

    new results

--- bin/settings.py
+++ bin/settings.py
@@ -5,25 +5,25 @@ have all the settings in a python file and import it? That is such a simple solu
 use_gpu = True  # Only works if you have tensorflow-gpu installed
 
 # DATA
-load_saved_array = True  # Loads array from file
+load_saved_array = False  # Loads array from file
 data_folder = '../data/training_data'  # Folders are choosen relative from NN.py
 equalize_data = True  # Makes amount of negative and positive data equal
 save_array = True  # Save array to file
 seq_length = 30  # Length of sequence that will be analysed
 data_augmentation = False  # Will divide
-fix_samples = 'ZERO'
+fix_samples = 'IGNORE'
 vectorize = True
 use_ascii = False
 
 # MODEL
-load_saved_model = True
+load_saved_model = False
 train_loaded_model = False
 choosen_model = 2  #
 
 # TRAINING DATA
 train_test_split = 0.8  # Splits full data set into train and test set, percentage train:test
 train_val_split = 0.8  # Splits train data set into train and validation set, percentage train:validation
-limit_samples = 6000  # None if you don't want to limit the amount of samples
+limit_samples = None  # None if you don't want to limit the amount of samples
 
 # TRAINING
 lr = 0.0001  # More parameters --> change lr
@@ -41,7 +41,7 @@ plot_performance = False
 
 # GAN
 train_GAN = True
-load_trained_GAN = False
+load_trained_GAN = True
 GAN_lr = 0.0001
 GAN_epochs = 3000
 GAN_batch_size = 512
